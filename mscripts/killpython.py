4927cdeb79fe27db46bf4becfd4bf481f34de215
Andrew <andrew.freelance@i.ua>
2016-03-09 19:37:11 +0200

    ...

--- mscripts/killpython.py
+++ mscripts/killpython.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 
 import os
 import subprocess
@@ -13,7 +13,7 @@ processes_info = map(lambda x: x[9:].strip(), processes_info)
 for process in processes_info:
     if seek_expr in process:
         process_pid = int(process.split(' ')[0])
-        if process_pid == my_pid: contimue
+        if process_pid == my_pid: continue
         os.kill(process_pid, 9)
 
 
