cb6a513d6d3715e990a32b804ac3236b55c4a253
Andrew <andrew.freelance@i.ua>
2016-03-09 19:30:25 +0200

    kill python was added

---
+++ mscripts/killpython.py
@@ -0,0 +1,19 @@
+!/usr/local/env python
+
+import os
+import subprocess
+
+my_pid = os.getpid()
+
+seek_expr = os.sys.argv[1]
+
+processes_info = subprocess.check_output(['ps', 'aux']).split('\n')[1:]
+processes_info = map(lambda x: x[9:].strip(), processes_info)
+
+for process in processes_info:
+    if seek_expr in process:
+        process_pid = int(process.split(' ')[0])
+        if process_pid == my_pid: contimue
+        os.kill(process_pid, 9)
+
+
